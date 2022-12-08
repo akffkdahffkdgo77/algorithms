98b08604aee3916a2d48a3990bc917c3ebd781ba
김가영 <namiein@naver.com>
2022-12-09 08:58:03 +0900

    solve : 구구단

---
+++ baekjoon/새싹/반복/22.js
@@ -0,0 +1,19 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: 새싹
+ *  Type : 반복
+ *  Problem : 2739
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .split('\n')[0];
+
+let answer = '';
+for (let i = 1; i <= 9; i++) {
+    answer += `${input} * ${i} = ${+input * i} \n`;
+}
+
+console.log(answer);
