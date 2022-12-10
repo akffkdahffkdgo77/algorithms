0514bfab423b47480a4742418727b05d03bbfc0d
김가영 <namiein@naver.com>
2022-12-10 22:01:57 +0900

    solve : day6

---
+++ aoc2022/day6/attempt.js
@@ -0,0 +1,33 @@
+const input = require('fs').readFileSync('./example.txt').toString().split('\n');
+
+// Part 1
+for (let i = 0; i < input.length; i++) {
+    const row = input[i].split('');
+    for (let j = 0; j < row.length; j++) {
+        if (j > 2) {
+            const arr = input[i].slice(j - 3, j + 1);
+            const obj = arr.split('').reduce((prev, cur) => ({ ...prev, [cur]: prev[cur] ? prev[cur] + 1 : 1 }), 0);
+            const isDuplicated = Object.keys(obj).some((key) => obj[key] > 1);
+            if (!isDuplicated) {
+                console.log('Part 1', j + 1);
+                break;
+            }
+        }
+    }
+}
+
+// Part 2
+for (let i = 0; i < input.length; i++) {
+    const row = input[i].split('');
+    for (let j = 0; j < row.length; j++) {
+        if (j > 12) {
+            const arr = input[i].slice(j - 13, j + 1);
+            const obj = arr.split('').reduce((prev, cur) => ({ ...prev, [cur]: prev[cur] ? prev[cur] + 1 : 1 }), 0);
+            const isDuplicated = Object.keys(obj).some((key) => obj[key] > 1);
+            if (!isDuplicated) {
+                console.log('Part 2', j + 1);
+                break;
+            }
+        }
+    }
+}
