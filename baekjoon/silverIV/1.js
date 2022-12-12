b74bac20636229e0e2970b66ae93d12bc06703cc
김가영 <namiein@naver.com>
2022-12-12 21:47:37 +0900

    solve : 나는야 포켓몬 마스터 이다솜

---
+++ baekjoon/silverIV/1.js
@@ -0,0 +1,27 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Silver IV
+ *  Problem : 1620
+ */
+
+const [first, ...rest] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .split('\n');
+const [N, M] = first.split(' ').map(Number);
+const pokemon = rest.slice(0, N);
+const problems = rest.slice(N, N + M);
+
+let answer = '';
+const list = new Map(pokemon.map((v, i) => [v, i + 1]));
+
+problems.forEach((prob) => {
+    if (!isNaN(Number(prob))) {
+        answer += `${pokemon[Number(prob) - 1]} \n`;
+    } else {
+        answer += `${list.get(prob)} \n`;
+    }
+});
+
+console.log(answer);
