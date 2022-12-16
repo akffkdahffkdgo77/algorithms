cfa792efdfd82aa9527b8b62f81fbcc2f8f2daa0
김가영 <namiein@naver.com>
2022-12-16 19:20:20 +0900

    solve : 엄청난 부자2

---
+++ baekjoon/bronzeV/1.js
@@ -0,0 +1,17 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 1271
+ */
+
+// 10^1000 -> BigInt
+const [N, M] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .split('\n')[0]
+    .split(' ')
+    .map(BigInt);
+
+console.log(`${N / M}`);
+console.log(`${N % M}`);
