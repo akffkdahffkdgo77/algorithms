923cb90a1e170227eafe0cd37fb51f94dfda27f6
김가영 <namiein@naver.com>
2022-12-16 19:21:51 +0900

    solve : Shares

---
+++ baekjoon/bronzeV/5.js
@@ -0,0 +1,25 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 3733
+ */
+
+// 10^1000 -> BigInt
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .split('\n');
+
+let answer = '';
+input.forEach((value) => {
+    if (value) {
+        const parsed = value.split(' ');
+        const N = BigInt(Number(parsed[0]) + 1);
+        const S = BigInt(parsed[1]);
+
+        answer += `${S / N} \n`;
+    }
+});
+
+console.log(answer.trim());
