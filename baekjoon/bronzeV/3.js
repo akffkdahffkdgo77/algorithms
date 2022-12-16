67a097a697020f623862c8a05eaaf37ef9e145cc
김가영 <namiein@naver.com>
2022-12-16 19:20:49 +0900

    solve : A+B - 2

---
+++ baekjoon/bronzeV/3.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 2558
+ */
+
+const [A, B] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+
+console.log(`${A + B}`);
