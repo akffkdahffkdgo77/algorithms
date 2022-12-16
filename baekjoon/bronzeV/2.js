a0b8496c9d98dde10864d997e6ec81ae1606818c
김가영 <namiein@naver.com>
2022-12-16 19:20:35 +0900

    solve : 긴자리 계산

---
+++ baekjoon/bronzeV/2.js
@@ -0,0 +1,17 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 2338
+ */
+
+// 10^1000 -> BigInt
+const [A, B] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .split('\n')
+    .map(BigInt);
+
+console.log(`${A + B}`);
+console.log(`${A - B}`);
+console.log(`${A * B}`);
