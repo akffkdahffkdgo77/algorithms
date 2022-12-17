30ada96a77c36756b222396354d74d2dec6b9ac8
김가영 <namiein@naver.com>
2022-12-17 22:21:51 +0900

    solve : 나부 함대 데이터

---
+++ baekjoon/bronzeV/20.js
@@ -0,0 +1,13 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 9654
+ */
+
+console.log('SHIP NAME      CLASS          DEPLOYMENT IN SERVICE');
+console.log('N2 Bomber      Heavy Fighter  Limited    21        ');
+console.log('J-Type 327     Light Combat   Unlimited  1         ');
+console.log('NX Cruiser     Medium Fighter Limited    18        ');
+console.log('N1 Starfighter Medium Fighter Unlimited  25        ');
+console.log('Royal Cruiser  Light Combat   Limited    4         ');
