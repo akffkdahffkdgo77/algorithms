2f2ae68f651c6dfdf6b8ae9ff41f1677617323e8
김가영 <namiein@naver.com>
2022-12-17 20:41:09 +0900

    solve : 웰컴

---
+++ baekjoon/bronzeV/8.js
@@ -0,0 +1,10 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 5337
+ */
+
+console.log('.  .   .');
+console.log('|  | _ | _. _ ._ _  _');
+console.log('|/\\|(/.|(_.(_)[ | )(/.');
