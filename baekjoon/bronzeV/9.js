774438e9ebd3d9eab9793ee42c4b8b8cdc75568a
김가영 <namiein@naver.com>
2022-12-17 20:44:24 +0900

    solve : 마이크로소프트 로고

---
+++ baekjoon/bronzeV/9.js
@@ -0,0 +1,12 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 5338
+ */
+
+console.log('       _.-;;-._');
+console.log("'-..-'|   ||   |");
+console.log("'-..-'|_.-;;-._|");
+console.log("'-..-'|   ||   |");
+console.log("'-..-'|_.-''-._|");
