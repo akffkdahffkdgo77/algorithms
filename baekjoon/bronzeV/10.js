15efc89f643910bfc75414d1fa66cacd3cf6ce78
김가영 <namiein@naver.com>
2022-12-17 20:51:53 +0900

    solve : 콜센터

---
+++ baekjoon/bronzeV/10.js
@@ -0,0 +1,17 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 5339
+ */
+
+console.log('     /~\\');
+console.log('    ( oo|');
+console.log('    _\\=/_');
+console.log('   /  _  \\');
+console.log('  //|/.\\|\\\\');
+console.log(' ||  \\ /  ||');
+console.log('============');
+console.log('|          |');
+console.log('|          |');
+console.log('|          |');
