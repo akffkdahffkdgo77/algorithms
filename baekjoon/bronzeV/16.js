99c822d684381d1a895ea2a13519764006d29a97
김가영 <namiein@naver.com>
2022-12-17 21:52:16 +0900

    solve : Julka

---
+++ baekjoon/bronzeV/16.js
@@ -0,0 +1,19 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 8437
+ */
+
+const [N, M] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(BigInt);
+
+const Klaudia = (N + M) / 2n;
+const Natalia = (N - M) / 2n;
+
+console.log(Klaudia.toString());
+console.log(Natalia.toString());
