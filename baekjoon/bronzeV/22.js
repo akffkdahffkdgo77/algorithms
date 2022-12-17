4147aed23ae693250fd4922332ebfe418b2251ce
김가영 <namiein@naver.com>
2022-12-17 22:25:22 +0900

    solve : Hook

---
+++ baekjoon/bronzeV/22.js
@@ -0,0 +1,11 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 10189
+ */
+
+console.log('#  # #### #### #  #');
+console.log('#### #  # #  # # #');
+console.log('#### #  # #  # # #');
+console.log('#  # #### #### #  #');
