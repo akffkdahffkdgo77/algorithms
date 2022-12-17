eea43b855f1ee86af69cb3a2354849e788192695
김가영 <namiein@naver.com>
2022-12-17 22:17:00 +0900

    solve : 스타워즈 로고

---
+++ baekjoon/bronzeV/19.js
@@ -0,0 +1,18 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 9653
+ */
+
+console.log('    8888888888  888    88888');
+console.log('   88     88   88 88   88  88');
+console.log('    8888  88  88   88  88888');
+console.log('       88 88 888888888 88   88');
+console.log('88888888  88 88     88 88    888888');
+console.log('');
+console.log('88  88  88   888    88888    888888');
+console.log('88  88  88  88 88   88  88  88');
+console.log('88 8888 88 88   88  88888    8888');
+console.log(' 888  888 888888888 88  88      88');
+console.log('  88  88  88     88 88   88888888');
