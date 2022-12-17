a461f40258a0b282d9bc7e992b18196499de32a4
김가영 <namiein@naver.com>
2022-12-17 22:24:03 +0900

    solve : NFC West vs North

---
+++ baekjoon/bronzeV/21.js
@@ -0,0 +1,20 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 10170
+ */
+
+console.log('NFC West       W   L  T');
+console.log('-----------------------');
+console.log('Seattle        13  3  0');
+console.log('San Francisco  12  4  0');
+console.log('Arizona        10  6  0');
+console.log('St. Louis      7   9  0');
+console.log('');
+console.log('NFC North      W   L  T');
+console.log('-----------------------');
+console.log('Green Bay      8   7  1');
+console.log('Chicago        8   8  0');
+console.log('Detroit        7   9  0');
+console.log('Minnesota      5  10  1');
