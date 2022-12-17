b75e7b955ee7c241e9caedfae3d436630a056250
김가영 <namiein@naver.com>
2022-12-17 22:52:57 +0900

    solve : ??!

---
+++ baekjoon/bronzeV/26.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 10926
+ */
+
+const ID = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(`${ID}??!`);
