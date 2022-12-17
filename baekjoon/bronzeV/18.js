f4f20136bd9f6d4a2fb3f25c262783ae63263da5
김가영 <namiein@naver.com>
2022-12-17 22:09:36 +0900

    solve : Zadanie próbne 2

---
+++ baekjoon/bronzeV/18.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 8871
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(`${2 * (Number(input) + 1)} ${3 * (Number(input) + 1)}`);
