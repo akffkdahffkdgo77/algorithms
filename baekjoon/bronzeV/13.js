bd11a36418e396db25d04d9c6881e30edc4c5c8c
김가영 <namiein@naver.com>
2022-12-17 21:10:09 +0900

    solve : Who is in the middle?

---
+++ baekjoon/bronzeV/13.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 6840
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number)
+    .sort((a, b) => a - b);
+
+console.log(input[1]);
