99d11022763ea1710ad7ff84bd745b99ad76c7a9
김가영 <namiein@naver.com>
2022-12-18 23:51:06 +0900

    solve : Correct

---
+++ baekjoon/bronzeV/70.js
@@ -0,0 +1,19 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 26307
+ */
+
+const [H, M] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0]
+    .split(' ')
+    .map(Number);
+
+const hours = H - 9;
+const minutes = M - 0;
+const answer = !hours && !minutes ? 0 : hours * 60 + minutes;
+console.log(answer);
