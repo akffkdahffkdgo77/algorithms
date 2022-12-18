e85a176e7d61a55e2ff7c349065739bd30dbaea7
김가영 <namiein@naver.com>
2022-12-18 11:21:01 +0900

    solve : 조별과제를 하려는데 조장이 사라졌다

---
+++ baekjoon/bronzeV/36.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 15727
+ */
+
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(Math.ceil(N / 5));
