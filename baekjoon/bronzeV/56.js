95b39024596a95e56d477e8b1e48d43bc7aa4568
김가영 <namiein@naver.com>
2022-12-18 21:05:32 +0900

    solve : 立方体 (Cube)

---
+++ baekjoon/bronzeV/56.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 24082
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(Math.pow(Number(input), 3));
