0be4d7441612fd956822ac5841deca1cfc4f9f7b
김가영 <namiein@naver.com>
2022-12-18 23:41:38 +0900

    solve : Intercepting Information

---
+++ baekjoon/bronzeV/69.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 26209
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0]
+    .split(' ')
+    .map(Number);
+
+console.log(input.filter((v) => v === 9).length ? 'F' : 'S');
