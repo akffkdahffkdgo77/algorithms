f23e0d105a41d69f078b1ff64c9f633ad8d349fe
김가영 <namiein@naver.com>
2022-12-18 20:25:38 +0900

    solve : 세금

---
+++ baekjoon/bronzeV/49.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 20492
+ */
+
+const prize = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(`${prize - prize * 0.22} ${prize - prize * 0.2 * 0.22}`);
