2ab3e746cc4e88778538017e4487f7132e8447f6
김가영 <namiein@naver.com>
2022-12-18 22:16:00 +0900

    solve : WARBOY

---
+++ baekjoon/bronzeV/68.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 26082
+ */
+
+const [A, B, C] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0]
+    .split(' ')
+    .map(Number);
+
+console.log(parseInt(B / A) * 3 * C);
