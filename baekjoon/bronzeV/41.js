2015e21203b5bf26eb88a8350f126043284c93dc
김가영 <namiein@naver.com>
2022-12-18 18:07:58 +0900

    solve : 오늘의 날짜는?

---
+++ baekjoon/bronzeV/41.js
@@ -0,0 +1,10 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 16170
+ */
+
+console.log(new Date().getUTCFullYear());
+console.log(new Date().getUTCMonth() + 1);
+console.log(new Date().getUTCDate());
