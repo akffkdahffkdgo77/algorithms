d76e29a1451da7653528487f57b708a326a045cb
김가영 <namiein@naver.com>
2022-12-18 21:27:38 +0900

    solve : Cupcake Party

---
+++ baekjoon/bronzeV/60.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 24568
+ */
+
+const [R, S] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+
+console.log(R * 8 + S * 3 - 28);
