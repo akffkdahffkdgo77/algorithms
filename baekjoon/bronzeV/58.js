b0e0e0222c689d0d6b4e7cebbe390a1d4a0ac442
김가영 <namiein@naver.com>
2022-12-18 21:21:59 +0900

    solve : 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ baekjoon/bronzeV/58.js
@@ -0,0 +1,27 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 24262
+ */
+
+// 시간 복잡도
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number)[0];
+
+if (N === N) {
+    console.log(N / N);
+    console.log(0);
+} else if (N === Math.pow(N, 2)) {
+    console.log(parseInt(N / Math.pow(N, 2)));
+    console.log(1);
+} else if (N === Math.pow(N, 3)) {
+    console.log(parseInt(N / Math.pow(N, 3)));
+    console.log(2);
+} else {
+    console.log(4);
+}
