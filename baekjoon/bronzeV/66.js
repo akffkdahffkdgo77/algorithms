5a13b4a8be810ebdfe7048c9f854d1eba16069c1
김가영 <namiein@naver.com>
2022-12-18 22:05:44 +0900

    solve : 코딩은 체육과목 입니다

---
+++ baekjoon/bronzeV/66.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 25314
+ */
+
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(`${'long '.repeat(parseInt(N / 4))}int`);
