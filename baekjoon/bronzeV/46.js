2cf63d9b8112116f98e1d0f3a2ae8169892a5159
김가영 <namiein@naver.com>
2022-12-18 19:53:32 +0900

    solve : 1998년생인 내가 태국에서는 2541년생?!

---
+++ baekjoon/bronzeV/46.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 18108
+ */
+
+const year = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(Number(year) - 543);
