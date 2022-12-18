142c7bc6bb5a7166e36654f67a38ba65db587dd8
김가영 <namiein@naver.com>
2022-12-18 11:10:24 +0900

    solve : Helicopter

---
+++ baekjoon/bronzeV/34.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 14337
+ *  제한 : Visual Basic
+ */
+
+console.log('      ===================');
+console.log('          ____||___');
+console.log('\\ /      /       []\\');
+console.log(' X=======           \\__');
+console.log('/ \\      \\____________|');
+console.log('            ||  ||');
+console.log('         \\-----------/');
