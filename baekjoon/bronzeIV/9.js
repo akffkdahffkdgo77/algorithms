6d14e5f5d74c4ca44d31a890433712b674a327b9
김가영 <namiein@naver.com>
2022-12-25 20:46:41 +0900

    solve : Celebrity jeopardy

---
+++ baekjoon/bronzeIV/9.js
@@ -0,0 +1,19 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 3765
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+let answer = '';
+input.forEach((val) => {
+    answer += `${val} \n`;
+});
+
+console.log(answer);
