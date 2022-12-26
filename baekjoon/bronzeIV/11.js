98992433c56be33425f292136f97c123a7d9cbf6
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2022-12-27 08:10:24 +0900

    solve : 줄번호

---
+++ baekjoon/bronzeIV/11.js
@@ -0,0 +1,20 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 4470
+ */
+
+const [N, ...input] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+let answer = "";
+input.forEach((val, index) => {
+    if (index < Number(N)) {
+        answer += `${index + 1}. ${val} \n`;
+    }
+});
+console.log(answer);
