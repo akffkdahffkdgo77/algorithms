99ee06686198a542ecf7f8d252fe6c60ba7953f0
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2022-12-27 09:09:08 +0900

    solve : 입실 관리

---
+++ baekjoon/bronzeIV/18.js
@@ -0,0 +1,20 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 5524
+ */
+
+const [N, ...input] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+let answer = "";
+input.forEach((val, index) => {
+    if (index < Number(N)) {
+        answer += `${val.toLowerCase()} \n`;
+    }
+});
+console.log(answer.trim());
