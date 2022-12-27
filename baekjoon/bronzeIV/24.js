e395365e9052d31b546e7e91fdf5209cbd96b81a
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2022-12-28 08:25:04 +0900

    solve : 상근이의 친구들

---
+++ baekjoon/bronzeIV/24.js
@@ -0,0 +1,22 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 5717
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+let answer = "";
+input.forEach((val) => {
+    const [M, F] = val.split(" ").map(Number);
+    if (M && F) {
+        answer += `${M + F} \n`;
+    }
+});
+
+console.log(answer.trim());
