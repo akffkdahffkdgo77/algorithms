27028ee94cec2d4beed9040ce5869165524f603d
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2022-12-29 08:06:43 +0900

    solve : Body Mass Index

---
+++ baekjoon/bronzeIV/32.js
@@ -0,0 +1,26 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 6825
+ */
+
+const [W, H] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+
+const BMI = W / (H * H);
+
+let answer = "";
+if (BMI > 25) {
+    answer = "Overweight";
+} else if (BMI >= 18.5) {
+    answer = "Normal weight";
+} else {
+    answer = "Underweight";
+}
+
+console.log(answer);
