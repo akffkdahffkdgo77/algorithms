e3bc3ccd80464b432a9c67f21e76ba350581422a
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2022-12-29 08:22:29 +0900

    solve : Squares

---
+++ baekjoon/bronzeIV/34.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 6887
+ */
+
+const N = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n")[0];
+
+console.log(`The largest square has side length ${Math.floor(Math.sqrt(N))}.`);
