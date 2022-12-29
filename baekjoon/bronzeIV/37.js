c5422bb274c35cb3b266729ca8dbd447e6529ded
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2022-12-29 09:24:12 +0900

    solve : Triangles

---
+++ baekjoon/bronzeIV/37.js
@@ -0,0 +1,21 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 7595
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+
+let answer = "";
+input.forEach((val) => {
+    if (val) {
+        Array.from({ length: val }).map((_, i) => (answer += `${"*".repeat(i + 1)}\n`));
+    }
+});
+console.log(answer);
