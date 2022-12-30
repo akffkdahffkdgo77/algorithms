ef014d1465d18d341f7707569b2fff5156da1620
김가영 <namiein@naver.com>
2022-12-30 13:56:09 +0900

    solve : 전자레인지

---
+++ baekjoon/bronzeIV/49.js
@@ -0,0 +1,26 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 10162
+ */
+
+const T = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+const A = 5 * 60;
+const B = 1 * 60;
+const C = 10;
+
+const totalA = Math.floor(T / A);
+const totalB = Math.floor((T % A) / B);
+const totalC = Math.floor((T % B) / C);
+
+if (totalA * A + totalB * B + totalC * C !== Number(T)) {
+    console.log(-1);
+} else {
+    console.log(totalA, totalB, totalC);
+}
