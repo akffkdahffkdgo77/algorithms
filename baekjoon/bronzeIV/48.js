dae1007d6175b6f48d229751db25875c38e1e081
김가영 <namiein@naver.com>
2022-12-30 12:31:34 +0900

    solve : 과자

---
+++ baekjoon/bronzeIV/48.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 10156
+ */
+
+const [K, N, M] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0]
+    .split(' ')
+    .map(Number);
+
+console.log(K * N - M > 0 ? K * N - M : 0);
