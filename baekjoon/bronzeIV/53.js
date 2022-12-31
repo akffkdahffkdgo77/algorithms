72aa8d8e00af0ef47cf923232a88a244df83bc42
김가영 <namiein@naver.com>
2022-12-31 10:24:51 +0900

    solve : 알파벳 개수

---
+++ baekjoon/bronzeIV/53.js
@@ -0,0 +1,26 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 10808
+ */
+
+const word = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0]
+    .split('');
+
+const obj = word.reduce((prev, cur) => ({ ...prev, [cur]: prev[cur] ? prev[cur] + 1 : 1 }), {});
+
+const answer = Array.from({ length: 27 }).reduce((prev, _, i) => {
+    const char = String.fromCharCode(i + 96);
+    return { ...prev, [char]: obj[char] ?? 0 };
+});
+
+console.log(
+    Object.values(answer)
+        .reduce((prev, cur) => (prev += `${cur} `), '')
+        .trim()
+);
