be9a76827da402b7ef2adb1da0626202694425c0
김가영 <namiein@naver.com>
2022-12-31 10:52:49 +0900

    solve : Communication Channels

---
+++ baekjoon/bronzeIV/54.js
@@ -0,0 +1,26 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 11121
+ */
+
+const [N, ...input] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+let answer = '';
+input.forEach((val, index) => {
+    if (index < N) {
+        const [A, B] = val.split(' ');
+        if (A === B) {
+            answer += 'OK\n';
+        } else {
+            answer += 'ERROR\n';
+        }
+    }
+});
+
+console.log(answer.trim());
