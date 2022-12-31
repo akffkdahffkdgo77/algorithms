6703a2d4f5503ce67e902f42ecce0bdceff81037
김가영 <namiein@naver.com>
2022-12-31 13:21:45 +0900

    solve : 파일 옮기기

---
+++ baekjoon/bronzeIV/60.js
@@ -0,0 +1,19 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 11943
+ */
+
+const [ASET, BSET] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+const [A, B] = ASET.split(' ').map(Number);
+const [C, D] = BSET.split(' ').map(Number);
+const firstMax = A + D;
+const secondMax = B + C;
+
+console.log(firstMax >= secondMax ? secondMax : firstMax);
