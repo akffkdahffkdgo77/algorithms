1d6ccbc6e2c9272db3c1d1f79d1dcd146a100382
김가영 <namiein@naver.com>
2022-12-31 09:55:16 +0900

    solve : 특별한 날

---
+++ baekjoon/bronzeIV/51.js
@@ -0,0 +1,17 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 10768
+ */
+
+const [M, D] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+const date1 = new Date(2015, M, D);
+const date2 = new Date(2015, 2, 18);
+
+console.log(date1 > date2 ? 'After' : date1 < date2 ? 'Before' : 'Special');
