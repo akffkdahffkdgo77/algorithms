91df8cd37e061ce28f51b7dad892b23f261fb237
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2023-01-02 08:22:12 +0900

    solve : 뉴턴과 사과

---
+++ baekjoon/bronzeIV/63.js
@@ -0,0 +1,26 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 13118
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+const [X] = input[1].split(" ").map(Number);
+
+let answer = "";
+input[0]
+    .split(" ")
+    .map(Number)
+    .forEach((val, index) => {
+        if (val === X) {
+            answer = index + 1;
+        }
+    });
+
+console.log(answer || 0);
