455c33a4673ecc345219726bb304402b48cfe1e0
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2023-01-02 09:58:25 +0900

    The Merchant of Venice

---
+++ baekjoon/bronzeIV/65.js
@@ -0,0 +1,34 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 13496
+ */
+
+const [N, ...input] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+let length = 0;
+let answer = "";
+for (let i = 0; i < Number(N); i++) {
+    const curIndex = i + length;
+    const [n, s, day] = input[curIndex].split(" ").map(Number);
+    answer += `Data Set ${i + 1}:\n`;
+
+    let total = 0;
+    Array.from({ length: n }).forEach((_, index) => {
+        if (input[curIndex + index + 1]) {
+            const [d, v] = input[curIndex + index + 1]?.split(" ").map(Number);
+            if (d / s <= day) {
+                total += v;
+            }
+        }
+    });
+    length += n;
+    answer += `${total}\n\n`;
+}
+
+console.log(answer.trim());
