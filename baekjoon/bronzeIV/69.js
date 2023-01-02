baeb2474eeef43fddd8492c502dea7263d2046a1
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2023-01-02 11:37:00 +0900

    solve : 팀 나누기

---
+++ baekjoon/bronzeIV/69.js
@@ -0,0 +1,23 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 13866
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n")[0]
+    .split(" ")
+    .map(Number);
+
+const max = Math.max(...input);
+const min = Math.min(...input);
+
+const sum = input.reduce((prev, cur) => (prev += cur), 0);
+const firstPair = max + min;
+const secondPair = sum - firstPair;
+
+console.log(Math.abs(secondPair - firstPair));
