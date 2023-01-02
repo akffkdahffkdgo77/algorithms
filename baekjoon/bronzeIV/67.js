b0f25da4a42313d6e312a704302da2d6af7298bb
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2023-01-02 10:50:25 +0900

    solve : Tri-du

---
+++ baekjoon/bronzeIV/67.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 13597
+ */
+
+const [A, B] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n")[0]
+    .split(" ")
+    .map(Number);
+
+console.log(Math.max(A, B));
