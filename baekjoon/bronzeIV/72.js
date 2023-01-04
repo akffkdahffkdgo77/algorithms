20a52623f87ead68b302b5897f96b8daacebc8b5
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2023-01-04 09:56:37 +0900

    solve : 치킨 두 마리 (...)

---
+++ baekjoon/bronzeIV/72.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 14489
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+const [A, B] = input[0].split(" ").map(Number);
+console.log(A + B >= Number(input[1]) * 2 ? A + B - Number(input[1]) * 2 : A + B);
