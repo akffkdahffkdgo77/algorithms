7390bcd6d59b186753aa39beb2736d357812a01c
김가영 <namiein@naver.com>
2023-01-08 18:41:07 +0900

    solve : Serca

---
+++ baekjoon/bronzeV/77.js
@@ -0,0 +1,28 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 26766
+ */
+
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+const first = ' @@@   @@@ ';
+const second = '@   @ @   @';
+const third = '@    @    @';
+const fourth = '@         @';
+const fifth = ' @       @ ';
+const sixth = '  @     @  ';
+const seventh = '   @   @   ';
+const eigth = '    @ @    ';
+const last = '     @     ';
+
+const arr = [first, second, third, fourth, fifth, sixth, seventh, eigth, last];
+
+let answer = '';
+Array.from({ length: Number(N) }).forEach(() => arr.forEach((val) => (answer += `${val}\n`)));
+console.log(answer);
