df2c6f68fe9ea069e8808bedc4c2aa918ba598ea
김가영 <namiein@naver.com>
2023-01-08 20:08:18 +0900

    solve : 전자레인지

---
+++ baekjoon/bronzeIV/79.js
@@ -0,0 +1,22 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 14470
+ */
+
+const [A, B, C, D, E] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+
+let time = 0;
+if (A < 0) {
+    time = Math.abs(A) * C + D + B * E;
+} else {
+    time = (B - A) * E;
+}
+
+console.log(time);
