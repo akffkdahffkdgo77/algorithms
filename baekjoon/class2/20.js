28d052837acfcbc3fabb46e0fc06b9d48e3ba429
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2023-01-09 09:18:16 +0900

    solve : 좌표 정렬하기

---
+++ baekjoon/class2/20.js
@@ -0,0 +1,27 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: CLASS 2
+ *  Problem : 11650
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+input.shift();
+
+input.sort((a, b) => {
+    const [x1, y1] = a.split(" ").map(Number);
+    const [x2, y2] = b.split(" ").map(Number);
+
+    if (x1 === x2) {
+        return y1 - y2;
+    }
+
+    return x1 - x2;
+});
+
+console.log(input.join("\n"));
