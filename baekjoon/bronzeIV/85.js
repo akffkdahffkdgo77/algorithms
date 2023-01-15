185cd4d200f0e2ab098f6c093c2921c5840af566
김가영 <namiein@naver.com>
2023-01-15 13:43:27 +0900

    solve : Rain Diary

---
+++ baekjoon/bronzeIV/85.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze IV
+ *  Problem : 27182
+ */
+
+const [N, R] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0]
+    .split(' ')
+    .map(Number);
+
+console.log(R + 7 > 28 ? N - 7 : R + 7);
