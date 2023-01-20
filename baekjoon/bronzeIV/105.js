a3bf0b6e3224dbe9cbb4990cee22c0f169f0f0e0
김가영 <namiein@naver.com>
2023-01-21 00:58:42 +0900

    solve : Contemporary Art

---
+++ baekjoon/bronzeIV/105.js
@@ -0,0 +1,16 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 16600
+ *  Algorithm : Math, Geometry
+ */
+
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number)[0];
+
+console.log(Math.sqrt(N) * 4);
