adfab269f55947ffd420649d5aebf5a18560e7b6
김가영 <namiein@naver.com>
2023-01-21 15:45:45 +0900

    solve : 母音を数える (Counting Vowels)

---
+++ baekjoon/bronzeIV/118.js
@@ -0,0 +1,26 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 18409
+ *  Algorithm : Implementation, String
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+input.shift();
+
+const obj = input[0].split('').reduce((prev, cur) => ({ ...prev, [cur]: prev[cur] ? prev[cur] + 1 : 1 }), {});
+const answer = Object.keys(obj).reduce((prev, cur) => {
+    if (['a', 'e', 'i', 'o', 'u'].includes(cur)) {
+        return obj[cur] + prev;
+    }
+
+    return prev;
+}, 0);
+
+console.log(answer);
