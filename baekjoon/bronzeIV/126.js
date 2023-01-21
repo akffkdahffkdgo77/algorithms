e4734470a7bec39581ff18a842bc8c220076dbe0
김가영 <namiein@naver.com>
2023-01-21 23:38:38 +0900

    solve : Archivist

---
+++ baekjoon/bronzeIV/126.js
@@ -0,0 +1,44 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 20232
+ *  Algorithm : Implementation
+ */
+
+const Y = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number)[0];
+
+const obj = {
+    1995: 'ITMO',
+    1996: 'SPbSU',
+    1997: 'SPbSU',
+    1998: 'ITMO',
+    1999: 'ITMO',
+    2000: 'SPbSU',
+    2001: 'ITMO',
+    2002: 'ITMO',
+    2003: 'ITMO',
+    2004: 'ITMO',
+    2005: 'ITMO',
+    2006: 'PetrSU, ITMO',
+    2007: 'SPbSU',
+    2008: 'SPbSU',
+    2009: 'ITMO',
+    2010: 'ITMO',
+    2011: 'ITMO',
+    2012: 'ITMO',
+    2013: 'SPbSU',
+    2014: 'ITMO',
+    2015: 'ITMO',
+    2016: 'ITMO',
+    2017: 'ITMO',
+    2018: 'SPbSU',
+    2019: 'ITMO'
+};
+
+console.log(obj[Y]);
