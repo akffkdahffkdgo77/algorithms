4fde44e99fccfff26faec9511f3654be40839c23
김가영 <namiein@naver.com>
2023-01-21 15:21:56 +0900

    solve : 3 つの整数 (Three Integers)

---
+++ baekjoon/bronzeIV/117.js
@@ -0,0 +1,20 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 18408
+ *  Algorithm : Implementation
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0]
+    .split(' ')
+    .map(Number);
+
+const obj = input.reduce((prev, cur) => ({ ...prev, [cur]: prev[cur] ? prev[cur] + 1 : 1 }), 0);
+const answer = Object.keys(obj).filter((key) => obj[key] > 1)[0];
+
+console.log(answer ? answer : input[0]);
