943212028d927f48541b43a278291297a360650f
김가영 <namiein@naver.com>
2023-01-24 00:27:09 +0900

    solve : Изгороди

---
+++ baekjoon/bronzeIV/101-/163.js
@@ -0,0 +1,16 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 24623
+ *  Algorithm : Math, Geometry
+ */
+
+const [N, A] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+
+console.log((180 - A) / 2 + A / 2);
