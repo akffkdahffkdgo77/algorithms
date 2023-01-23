8da8f27e3d30ff48c952de15ed427bfb3cc68419
김가영 <namiein@naver.com>
2023-01-23 22:52:35 +0900

    solve : 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ baekjoon/bronzeIV/101-/159.js
@@ -0,0 +1,16 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 24263
+ *  Algorithm : Implementation, Simulation
+ */
+
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')[0];
+
+console.log(N);
+console.log(1);
