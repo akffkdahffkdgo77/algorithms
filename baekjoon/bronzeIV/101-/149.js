b93fb6a94de5750bec76ef0bee8ee910b0bc2b77
김가영 <namiein@naver.com>
2023-01-23 20:48:35 +0900

    solve : Arm Coordination

---
+++ baekjoon/bronzeIV/101-/149.js
@@ -0,0 +1,26 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 23375
+ *  Algorithm : Math, Geometry, Arithmetic Operations
+ */
+
+// 예제 무시해야 함
+const [coords, r] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+const [x, y] = coords.split(' ').map(Number);
+
+const firstX = Number(r) + x;
+const secondX = x - Number(r);
+const firstY = Number(r) + y;
+const secondY = y - Number(r);
+
+console.log(firstX, firstY);
+console.log(firstX, secondY);
+console.log(secondX, secondY);
+console.log(secondX, firstY);
