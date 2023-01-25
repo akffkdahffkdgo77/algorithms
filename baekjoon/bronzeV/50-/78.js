99d4c32cf5d405e02f7a8b4215e7bd0c2a97c2ca
김가영 <namiein@naver.com>
2023-01-25 15:25:58 +0900

    solve : Робинзон Крузо

---
+++ baekjoon/bronzeV/50-/78.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 27219
+ */
+
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim();
+
+const div = Math.floor(N / 5);
+const remainder = N % 5;
+
+console.log(`${'V'.repeat(div)}${'I'.repeat(remainder)}`);
