8fd2ebc19f793a1d63802ccaae3e5cc2c7adf21e
김가영 <namiein@naver.com>
2023-01-26 23:47:51 +0900

    solve : ATM

---
+++ baekjoon/silverIV/11399.js
@@ -0,0 +1,27 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Silver V
+ *  Problem : 11399
+ *  Algorithm : 그리디 알고리즘, 정렬
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+input.shift();
+
+let waitingTime = 0;
+const answer = input[0]
+    .split(' ')
+    .map(Number)
+    .sort((a, b) => a - b)
+    .reduce((prev, cur) => {
+        const sum = prev + cur + waitingTime;
+        waitingTime += cur;
+        return sum;
+    }, 0);
+
+console.log(answer);
