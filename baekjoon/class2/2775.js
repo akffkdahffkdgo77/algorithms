a3bd06c94817c8b1329b661e26945b847b5c20a2
김가영 <namiein@naver.com>
2023-01-28 21:48:20 +0900

    solve : 부녀회장이 될테야

---
+++ baekjoon/class2/2775.js
@@ -0,0 +1,42 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: CLASS 2
+ *  Problem : 2775
+ *  Algorithm : 수학, 구현, 다이나믹 프로그래밍
+ */
+
+const [n, ...input] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+
+const arr = [Array.from({ length: 14 }).map((_, i) => i + 1)];
+
+// 모든 방(15 * 15) 에 있는 사람들 수 계산
+for (let i = 0; i < 14; i++) {
+    const people = [];
+    for (let j = 0; j < 14; j++) {
+        let sum = 0;
+        for (let k = 0; k <= j; k++) {
+            sum += arr[i][k];
+        }
+        people.push(sum);
+    }
+    arr.push(people);
+}
+
+// k-1층 1호 ~ n호까지 사람들의 수의 합 구하기
+let answer = '';
+for (let i = 0; i < input.length; i += 2) {
+    const [k, n] = input.slice(i, i + 2);
+    let people = 0;
+    for (let j = 1; j <= n; j++) {
+        people += arr[k - 1][j - 1];
+    }
+    answer += `${people}\n`;
+}
+
+console.log(answer.trim());
