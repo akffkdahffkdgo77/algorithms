9292ad23413f373251fe758844f55220890633f9
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2023-02-02 10:02:20 +0900

    solve : 長方形 (Rectangle)

---
+++ baekjoon/bronzeV/50-/81.js
@@ -0,0 +1,16 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Problem Number: 27323
+ *  Level: Bronze V
+ *  Algorithm: Math, Geometry, Arithmetic Operations
+ */
+
+const [A, B] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+
+console.log(A * B);
