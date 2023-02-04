31267be5208fa111d33529b8628c366107b17a40
김가영 <namiein@naver.com>
2023-02-04 23:33:22 +0900

    solve : 三方比較 (Three-Way Comparison)

---
+++ baekjoon/bronzeV/50-/83.js
@@ -0,0 +1,16 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 27328
+ *  Algorithm: Math, Implementation, Arithmetic Operations
+ */
+
+const [A, B] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+
+console.log(A < B ? -1 : A === B ? 0 : 1);
