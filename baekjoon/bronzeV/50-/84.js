2d1a952b03f2395743a982051c5c3ab74b25fd73
김가영 <namiein@naver.com>
2023-02-05 13:43:25 +0900

    solve : 2 桁の整数 (Two-digit Integer)

---
+++ baekjoon/bronzeV/50-/84.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 27331
+ *  Algorithm: Math, Implementation, Arithmetic Operations
+ */
+
+const [A, B] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+console.log(Number(A + B));
