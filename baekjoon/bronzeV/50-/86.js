bbfe1daeff6f3958650dcb626eea3eed2b7b6921
김가영 <namiein@naver.com>
2023-02-07 22:43:26 +0900

    solve : Metronome

---
+++ baekjoon/bronzeV/50-/86.js
@@ -0,0 +1,14 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 27389
+ *  Algorithm: Math, Arithmetic Operations
+ */
+
+const N = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim();
+
+console.log((Number(N) / 4).toFixed(2));
