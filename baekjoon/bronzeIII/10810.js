810253768f95ddaa0f5ead5aaa453d0df62b2b09
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-03 08:53:12 +0900

    solve : 공 넣기

---
+++ baekjoon/bronzeIII/10810.js
@@ -0,0 +1,26 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 10810
+ *  Algorithm: Implementation, Simulation
+ */
+
+const [num, ...list] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+const [N, M] = num.split(" ").map(Number);
+const arr = [...Array(N).keys()].map(() => 0);
+
+for (let idx = 0; idx < list.length; idx++) {
+    const [i, j, k] = list[idx].split(" ").map(Number);
+
+    for (let jdx = i - 1; jdx < j; jdx++) {
+        arr[jdx] = k;
+    }
+}
+
+console.log(arr.join(" "));
