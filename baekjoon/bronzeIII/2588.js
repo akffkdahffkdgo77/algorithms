58d16d26693357b6b6593d9caba6cc8f22fa7b0e
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-03 08:04:57 +0900

    solve : 곱셈

---
+++ baekjoon/bronzeIII/2588.js
@@ -0,0 +1,20 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze III
+ *  Problem : 2588
+ *  Algorithm: Math, Arithmetic Operations
+ */
+
+const [first, second] = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+const secondNum = second.split("");
+
+for (let i = secondNum.length - 1; i >= 0; i--) {
+    console.log(Number(first) * secondNum[i]);
+}
+console.log(Number(first) * Number(second));
