c8c214fd86e2c74672e8dee087eec834bf60f717
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-02 13:26:48 +0900

    solve : Lucky 7

---
+++ baekjoon/bronzeV/50-/30224.js
@@ -0,0 +1,27 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 30224
+ *  Algorithm: Math, Implementation
+ */
+
+const N = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../../input.txt")
+    .toString()
+    .trim();
+
+const num = Number(N);
+if (N.includes("7")) {
+    if (num % 7 === 0) {
+        console.log(3);
+    } else {
+        console.log(2);
+    }
+} else {
+    if (num % 7 === 0) {
+        console.log(1);
+    } else {
+        console.log(0);
+    }
+}
