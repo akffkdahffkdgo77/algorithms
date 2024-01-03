6c5b198009cdc72f570f699bb1034602c2a7a4cf
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-03 20:30:39 +0900

    solve : 다이얼

---
+++ baekjoon/bronzeII/5622.js
@@ -0,0 +1,25 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze II
+ *  Problem : 5622
+ *  Algorithm: Implementation
+ */
+
+const wordList = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('');
+
+const alphabets = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz', 'operator'];
+
+let sum = 0;
+for (let i = 0; i < wordList.length; i++) {
+    if (wordList.includes('operator')) {
+        sum += 11;
+    } else {
+        sum += 3 + alphabets.findIndex((val) => val.includes(wordList[i].toLowerCase()));
+    }
+}
+console.log(sum);
