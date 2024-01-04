88bb4436790573dc45960042214ef75c7e880a41
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-05 00:07:56 +0900

    solve : 세로읽기

---
+++ baekjoon/bronzeI/10798.js
@@ -0,0 +1,30 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze I
+ *  Problem : 10798
+ *  Algorithm: Implementation, String
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+let answer = [];
+for (let i = 0; i < 15; i++) {
+    let arr = [];
+    for (let j = 0; j < input.length; j++) {
+        const row = input[j].split('');
+        if (row[i]) {
+            arr.push(row[i]);
+        }
+    }
+    if (arr.length) {
+        answer.push(arr);
+        arr = [];
+    }
+}
+
+console.log(answer.flat().join(''));
