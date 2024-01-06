ebcb0e9371c88e22079916bfbea93f24e5fdb2db
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-06 21:23:46 +0900

    solve : 소인수분해

---
+++ baekjoon/bronzeI/11653.js
@@ -0,0 +1,27 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze I
+ *  Problem : 11653
+ *  Algorithm: Math, Number Theory, Prime Number
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../input.txt')
+    .toString()
+    .trim();
+
+let num = Number(input);
+const answer = [];
+let divisor = 2;
+
+// 소인수분해
+while (num >= 2) {
+    if (num % divisor === 0) {
+        num = num / divisor;
+        answer.push(divisor);
+    } else {
+        divisor++;
+    }
+}
+console.log(answer.join('\n'));
