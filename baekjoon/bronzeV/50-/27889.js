0e7c834fcd142c47e9a989eb1dff139f72834e8f
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-16 07:52:13 +0900

    solve : 특별한 학교 이름

---
+++ baekjoon/bronzeV/50-/27889.js
@@ -0,0 +1,21 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 27889
+ *  Algorithm: Implementation, String
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../../input.txt")
+    .toString()
+    .trim();
+
+const obj = {
+    NLCS: "North London Collegiate School",
+    BHA: "Branksome Hall Asia",
+    KIS: "Korea International School",
+    SJA: "St. Johnsbury Academy"
+};
+
+console.log(obj[input]);
