3322daeb80685ad8fda257648d3a5c614ea58a6c
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-21 10:36:09 +0900

    solve : 정보보호학부 동아리 소개

---
+++ baekjoon/bronzeV/50-/28691.js
@@ -0,0 +1,22 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 28691
+ *  Algorithm: Implementation, String
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim();
+
+const obj = {
+    M: 'MatKor',
+    W: 'WiCys',
+    C: 'CyKor',
+    A: 'AlKor',
+    $: '$clear'
+};
+
+console.log(obj[input]);
