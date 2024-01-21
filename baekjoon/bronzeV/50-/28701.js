eee930a63a982c77808f9ca3a69acabc4f768e26
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-22 07:51:01 +0900

    solve : 세제곱의 합

---
+++ baekjoon/bronzeV/50-/28701.js
@@ -0,0 +1,23 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 28701
+ *  Algorithm: Math, Implementation, Arithmetic Operations
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../../input.txt")
+    .toString()
+    .trim();
+const num = Number(input);
+
+let sum = 0;
+let sum2 = 0;
+for (let i = 1; i <= num; i++) {
+    sum += i;
+    sum2 += Math.pow(i, 3);
+}
+console.log(sum);
+console.log(Math.pow(sum, 2));
+console.log(sum2);
