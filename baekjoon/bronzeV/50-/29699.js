6a23f2ffc8547f7dba9e80deb51169e5201501d6
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-24 07:53:53 +0900

    solve : Welcome to SMUPC!

---
+++ baekjoon/bronzeV/50-/29699.js
@@ -0,0 +1,18 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 29699
+ *  Algorithm: Math, Implementation, String, Arithmetic Operations
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../../input.txt")
+    .toString()
+    .trim();
+
+const num = Number(input);
+const word = "WelcomeToSMUPC";
+const index = num % word.length || 14;
+
+console.log(word.split("")[index - 1]);
