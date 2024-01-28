cca3ffe593549040ddf7f1100bebea62304f183b
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-01-28 19:53:59 +0900

    solve : 스위트콘 가격 구하기

---
+++ baekjoon/bronzeV/50-/30030.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 30030
+ *  Algorithm: Math, Arithmetic Operations
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim();
+
+const num = Number(input);
+console.log(Math.ceil(num / 1.1));
