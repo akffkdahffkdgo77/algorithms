ca19c43bf61732797ba9dd60b591f26f552aafce
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-02-01 07:53:50 +0900

    solve : Java Warriors

---
+++ baekjoon/bronzeV/50-/30328.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 30328
+ *  Algorithm: Math, Arithmetic Operations
+ */
+
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "../../../input.txt")
+    .toString()
+    .trim();
+
+const num = Number(input);
+console.log(num * 4000);
