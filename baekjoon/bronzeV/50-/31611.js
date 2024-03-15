ac1995b3cf1603b8ffc2e33036ea459d93de9d8b
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-03-15 20:09:12 +0900

    solve : 火曜日 (Tuesday)

---
+++ baekjoon/bronzeV/50-/31611.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 31611
+ *  Algorithm: Math, Implementation, Arithmetic Operations
+ */
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim();
+
+const num = Number(input);
+console.log(num === 2 || (num - 2) % 7 === 0 ? 1 : 0);
