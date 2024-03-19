4d54a4e0b0aedb7f376b19d9d89dee946af05cfc
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-03-19 19:14:33 +0900

    solve : Конвейер

---
+++ baekjoon/bronzeV/50-/28938.js
@@ -0,0 +1,20 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/
+ *  Level: Bronze V
+ *  Problem : 28938
+ *  Algorithm: Math, Implementation, Arithmetic Operations
+ */
+
+const [N, input] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+const num = input
+    .split(' ')
+    .map(Number)
+    .reduce((prev, cur) => prev + cur, 0);
+
+console.log(num > 0 ? 'Right' : num < 0 ? 'Left' : 'Stay');
