2d500b945dd68bee1716a00dde7605ae9336c2dd
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2024-04-02 21:33:12 +0900

    solve : Vowel Count

---
+++ baekjoon/bronzeIV/201-/26314.js
@@ -0,0 +1,28 @@
+/*
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net
+ *  Level : Bronze IV
+ *  Problem : 26314
+ *  Algorithm : Implementation, String
+ */
+
+const [N, ...input] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+for (let i = 0; i < input.length; i++) {
+    const list = input[i].split('');
+    let vowels = 0;
+    let consonants = 0;
+    for (let j = 0; j < list.length; j++) {
+        if (['a', 'e', 'i', 'o', 'u'].includes(list[j])) {
+            vowels++;
+        } else {
+            consonants++;
+        }
+    }
+    console.log(input[i]);
+    console.log(vowels > consonants ? 1 : 0);
+}
